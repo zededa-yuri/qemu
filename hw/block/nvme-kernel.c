4dfdaf236c73bea2c06e7907f8cb164c1cf5bb62
Vitaliy Kuznetsov <ahil52_25@mail.ru>
2021-07-23 18:22:36 +0300

    Temporarily change the conditions for nvme_mmio_write

--- hw/block/nvme-kernel.c
+++ hw/block/nvme-kernel.c
@@ -724,8 +724,9 @@ static void nvme_mmio_write(void *opaque, hwaddr addr, uint64_t data,
 
     trace_pci_nvme_mmio_write(addr, data);
 
-    nvme_write_bar(n, addr, data, size);
-    if (addr > sizeof(n->bar)) {
+    if (addr < sizeof(n->bar)) {
+        nvme_write_bar(n, addr, data, size);
+    } else {
         nvme_process_db(n, addr, data);
     }
 }
